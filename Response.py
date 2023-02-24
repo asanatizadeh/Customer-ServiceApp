0bc442fefdf299fd4a34c1a20802c0b7cdacef4e
asanatizadeh <83368940+asanatizadeh@users.noreply.github.com>
2023-02-24 12:03:02 -0600

    Update Response.py

--- Response.py
+++ Response.py
@@ -16,7 +16,7 @@ def generate_repy(review):
     max_tokens=256,
     top_p=1,
     frequency_penalty=0,
-    presence_penalty=0
+    presence_penalty=0,
     stop: None
   )
   return response.choices[0].text
